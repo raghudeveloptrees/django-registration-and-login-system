dc4832a5512d95f3402365ff64f92a9b0f5c4ede
raghudeveloptrees <151159310+raghudeveloptrees@users.noreply.github.com>
2024-01-14 23:32:43 +0530

    Update settings.py

--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://loginreg.azurewebsites.net']
 
 
 # Application definition
